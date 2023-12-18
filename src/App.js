b2d3dee53d55228da0dd8d276cc54ee04bf69aaa
Athar <official.atharellahi@gmail.com>
2023-12-18 17:20:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './components/styles/index.css'
+import data from './components/data/data.json'
+
+const Translation = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='parent'>
+        {data.map((item, index) => {
+
+          return (
+            <div className='table'>
+              <div className='index'>
+                <span className='value'>{item.ID} </span><span className='label'>Verse</span>
+              </div>
+              <div className='arabic'>
+                <span className='value'>{item.Arabic}</span>  <span className='label'>Arabic</span>
+              </div>
+              <div className='urdu'>
+                <span className='value'>{item.Traslation} </span><span className='label'>Urdu</span>
+              </div>
+              {item.English ? <div className='english'>
+                <span className='value'>{item.English}</span>  <span className='label'>English</span>
+              </div> : null}
+            </div>
+          )
+        })
+
+        }
+      </div>
+
+    </>
+  )
 }
 
-export default App;
+export default Translation;
