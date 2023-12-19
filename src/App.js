6d471c043cbdc959de47df408cac7d4a3da23b1f
Athar <official.atharellahi@gmail.com>
2023-12-20 01:46:24 +0530

    update

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const Translation = () => {
   const handlegottotop = () => {
     const bdy = document.querySelector('body')
     window.scrollTo({ top: 0, behavior: 'smooth' });
-    bdy.scrollTo({ top: 0, behavior: 'smooth' });
+    bdy.scrollTo({ top: 0, behavior: 'smooth' }); 
 
   }
 
